766e907bbb7b7e89881c3b8bb1fbf50ea327389a
= <webdev.jameslemire@gmail.com>
2017-06-20 10:11:54 -0600

    JL solution - Update solution.

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use( bodyParser.json() );
 app.use( express.static( __dirname + '/../public/build' ) );
 
-const baseURL = "/api/messages";
-app.post( baseURL, mc.create );
-app.get( baseURL, mc.read );
-app.put( `${baseURL}/:id`, mc.update );
-app.delete( `${baseURL}/:id`, mc.delete );
+const messagesBaseUrl = "/api/messages";
+app.post( messagesBaseUrl, mc.create );
+app.get( messagesBaseUrl, mc.read );
+app.put( `${messagesBaseUrl}/:id`, mc.update );
+app.delete( `${messagesBaseUrl}/:id`, mc.delete );
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
